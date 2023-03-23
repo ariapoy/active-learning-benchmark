e39fb5f42d51b8e40b044b837b358ddbae4160a6
ariapoy <ariapoy@gmail.com>
2023-03-23 21:23:07 +0800

    add hintsvm-alignG-zhan

--- src/run-alhyper.sh
+++ src/run-alhyper.sh
@@ -12,8 +12,7 @@ TOOL="libact";
 # hintsvm with aligned G
 QS="hintsvm-alignG-zhan"
 QUERY="libact-zhan";
-# for data in "appendicitis" "sonar" "parkinsons" "ex8b" "heart" "haberman" "ionosphere" "clean1" "breast" "wdbc" "australian" "diabetes" "mammographic" "ex8a" "tic" "german" "splice" "gcloudb" "gcloudub" "checkerboard"; do
-for data in "appendicitis" "sonar" "parkinsons"; do
+for data in "appendicitis" "sonar" "parkinsons" "ex8b" "heart" "haberman" "ionosphere" "clean1" "breast" "wdbc" "australian" "diabetes" "mammographic" "ex8a" "tic" "german" "splice" "gcloudb" "gcloudub" "checkerboard"; do
     echo "Start $QS with $HS of $data on $EXP with $N_JOBS"
     for s in $(seq $SEED $N_JOBS $END); do #
         timeout 25920 python main.py --tool $TOOL --qs_name $QS --hs_name $QUERY --gs_name $TASK --seed $s --n_jobs $N_JOBS --n_trials $N_JOBS --data_set $data;
