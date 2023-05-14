0a410a25549368b29b2ae096529bcd4c6b22a1f0
ariapoy <ariapoy@gmail.com>
2023-05-15 02:21:17 +0800

    fix unexecuted LRxall

--- src/run-LR_all_margin.sh
+++ src/run-LR_all_margin.sh
@@ -10,14 +10,16 @@ QS="margin-zhan";
 # QUERY model
 QUERY="LR";
 # Task model
-TASK="LR";
 
 # margin-zhan
-# for data in "appendicitis" "sonar" "parkinsons" "ex8b" "heart" "haberman" "ionosphere" "clean1" "breast" "wdbc" "australian" "diabetes" "mammographic" "ex8a" "tic" "german" "splice" "gcloudb" "gcloudub" "checkerboard"; do
-# for data in "ex8a"; do
-for data in "sonar" "haberman" "ionosphere" "tic" "gcloudb" "appendicitis" "checkerboard" "breast"; do
-    echo "Start $QS with $HS of $data on $EXP with $N_JOBS"
-    for s in $(seq $SEED $N_JOBS $END); do #
-        timeout 25920 python main.py --tool $TOOL --qs_name $QS --hs_name $QUERY --gs_name $TASK --seed $s --n_jobs $N_JOBS --n_trials $N_JOBS --data_set $data;
-    done
+# for TASK in "RBFSVM" "RandomForest" "LR"; do
+for TASK in "RBFSVM" "RandomForest"; do
+  # for data in "appendicitis" "sonar" "parkinsons" "ex8b" "heart" "haberman" "ionosphere" "clean1" "breast" "wdbc" "australian" "diabetes" "mammographic" "ex8a" "tic" "german" "splice" "gcloudb" "gcloudub" "checkerboard"; do
+  # for data in "ex8a"; do
+  for data in "sonar" "haberman" "ionosphere" "tic" "gcloudb" "appendicitis" "checkerboard" "breast"; do
+      echo "Start $QS with $HS of $data on $EXP with $N_JOBS"
+      for s in $(seq $SEED $N_JOBS $END); do #
+          timeout 25920 python main.py --tool $TOOL --qs_name $QS --hs_name $QUERY --gs_name $TASK --seed $s --n_jobs $N_JOBS --n_trials $N_JOBS --data_set $data;
+      done
+  done
 done
