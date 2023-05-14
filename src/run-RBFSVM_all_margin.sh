aa232481f00692c596675aea441d68cda2cb8af5
d09944015 <d09944015@linux2.csie.ntu.edu.tw>
2023-05-15 01:55:16 +0800

    add more exps for SVM(RBF)xall

--- src/run-RBFSVM_all_margin.sh
+++ src/run-RBFSVM_all_margin.sh
@@ -2,7 +2,7 @@
 SEED="0";
 N_EXP="150";
 # For multi-processing
-N_JOBS="5";
+N_JOBS="10";
 END=$(($SEED + $N_EXP - 1));
 TOOL="google";
 QS="margin-zhan";
@@ -14,7 +14,8 @@ QUERY="RBFSVM";
 ## 3.3 margin-zhan
 for TASK in "RandomForest" "LR"; do
   # for data in "appendicitis" "sonar" "parkinsons" "ex8b" "heart" "haberman" "ionosphere" "clean1" "breast" "wdbc" "australian" "diabetes" "mammographic" "ex8a" "tic" "german" "splice" "gcloudb" "gcloudub" "checkerboard"; do
-  for data in "ex8a"; do
+  # for data in "ex8a"; do
+  for data in "sonar" "haberman" "ionosphere" "tic" "gcloudb" "appendicitis" "checkerboard" "breast"; do
     echo "Start $QS with $HS of $data on $EXP with $N_JOBS"
     for s in $(seq $SEED $N_JOBS $END); do #
       timeout 25920 python main.py --tool $TOOL --qs_name $QS --hs_name $QUERY --gs_name $TASK --seed $s --n_jobs $N_JOBS --n_trials $N_JOBS --data_set $data;
