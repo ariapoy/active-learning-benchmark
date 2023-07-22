4690402eb4c22087eea9158466933b2b64c16b71
ariapoy <ariapoy@gmail.com>
2023-07-23 01:47:46 +0800

    update exps for benchmark with epsUS

---
+++ src/run-benchmark.sh
@@ -0,0 +1,30 @@
+# exp settings
+SEED="0";
+N_EXP="150";
+N_JOBS="1";
+# For multi-processing
+END=$(($SEED + $N_EXP - 1));
+
+# small datasets
+for EPS in "0.1" "0.2" "0.3"; do
+  for data in "appendicitis" "sonar" "parkinsons" "ex8b" "heart" "haberman" "ionosphere" "clean1" "breast" "wdbc" "australian" "diabetes" "mammographic" "ex8a" "tic" "german" "splice" "gcloudb" "gcloudub" "checkerboard"; do
+      echo "benchmark ($data, $EPS)"
+    for s in $(seq $SEED $N_JOBS $END); do #
+      timeout 25920 python main.py --qs_name eps_greedy --data_set $data --tool libact --hs_name google-zhan --gs_name zhan --seed $s --n_jobs $N_JOBS --n_trials $N_JOBS --eps $EPS
+    done
+  done
+done
+
+# large datasets
+SEED="0";
+N_EXP="15";
+# For multi-processing
+END=$(($SEED + $N_EXP - 1));
+for EPS in "0.1" "0.2" "0.3"; do
+  for data in "spambase" "banana" "phoneme" "ringnorm" "twonorm" "phishing"; do
+      echo "benchmark ($data, $EPS)"
+    for s in $(seq $SEED $N_JOBS $END); do #
+      timeout 259200 python main.py --qs_name eps_greedy --data_set $data --tool libact --hs_name google-zhan --gs_name zhan --seed $s --n_jobs $N_JOBS --n_trials $N_JOBS --eps $EPS
+    done
+  done
+done
