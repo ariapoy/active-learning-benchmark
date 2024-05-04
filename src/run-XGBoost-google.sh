ccd068aaf93c8ae83bb343ccf1f35d3c0e893341
d09944015 <d09944015@ws5.csie.ntu.edu.tw>
2024-05-04 16:43:37 +0800

    update exps run shell

--- src/run-XGBoost-google.sh
+++ src/run-XGBoost-google.sh
@@ -2,23 +2,16 @@
 TASK="XGBoost";  # task-oriented model
 QUERY="XGBoost";  # query-oriented model
 SEED="0";  # starting seed
-N_EXP="150";  # number of experiments
-# For multi-processing
-N_JOBS="1";  # number of process, depending on your resources and algorithms
+N_EXP="110";  # number of experiments
 END=$(($SEED + $N_EXP - 1));  # ending seed
+# For multi-processing
+N_JOBS="10";  # number of process, depending on your resources and algorithms
 
-# google
 TOOL="google";
 
 echo "Small datasets!"
-# uniform-zhan
-# margin-zhan
-# hier-zhan
-# mcm-zhan
-# graph-zhan
-# infodiv-zhan
-for data in "appendicitis" "sonar" "parkinsons" "ex8b" "heart" "haberman" "ionosphere" "clean1" "breast" "wdbc" "australian" "diabetes" "mammographic" "ex8a" "tic" "german" "splice" "gcloudb" "gcloudub" "checkerboard"; do
-  for QS in "uniform-zhan" "margin-zhan" "hier-zhan" "mcm-zhan" "graph-zhan" "infodiv-zhan"; do
+for QS in "uniform-zhan" "margin-zhan" "hier-zhan" "mcm-zhan" "graph-zhan" "infodiv-zhan"; do
+  for data in "appendicitis" "sonar" "parkinsons" "ex8b" "heart" "haberman" "ionosphere" "clean1" "breast" "wdbc" "australian" "diabetes" "mammographic" "ex8a" "tic" "german" "splice" "gcloudb" "gcloudub" "checkerboard"; do
     echo "Start $QS with $QUERY on $data, repeated $N_EXP times with $N_JOBS process."
       for s in $(seq $SEED $N_JOBS $END); do #
         timeout 25920 python main.py --data_set $data --qs_name $QS --hs_name $QUERY --gs_name $TASK --seed $s --n_jobs $N_JOBS --n_trials $N_JOBS --tool $TOOL;
@@ -27,11 +20,14 @@ for data in "appendicitis" "sonar" "parkinsons" "ex8b" "heart" "haberman" "ionos
 done
 
 echo "Large datasets!"
-for data in "spambase" "banana" "phoneme" "ringnorm" "twonorm" "phishing"; do
-  for QS in "uniform-zhan" "margin-zhan" "hier-zhan" "mcm-zhan" "graph-zhan" "infodiv-zhan"; do
-    echo "Start $QS with $QUERY on $data, repeated $N_EXP times with $N_JOBS process."
+N_EXP="10";  # number of experiments
+END=$(($SEED + $N_EXP - 1));  # ending seed
+BUDGET="3000";
+for QS in "uniform-zhan" "margin-zhan" "hier-zhan" "mcm-zhan" "graph-zhan" "infodiv-zhan"; do
+  for data in "spambase" "banana" "phoneme" "ringnorm" "twonorm" "phishing" "pol" "bioresponse" "covertype"; do
+    echo "Start $QS with $QUERY on $data limited budget $BUDGET, repeated $N_EXP times with $N_JOBS process."
       for s in $(seq $SEED $N_JOBS $END); do #
-        timeout 259200 python main.py --data_set $data --qs_name $QS --hs_name $QUERY --gs_name $TASK --total_budget 3000 --seed $s --n_jobs $N_JOBS --n_trials $N_JOBS --tool $TOOL;
+        timeout 259200 python main.py --data_set $data --qs_name $QS --total_budget $BUDGET --hs_name $QUERY --gs_name $TASK --seed $s --n_jobs $N_JOBS --n_trials $N_JOBS --tool $TOOL;
       done
   done
 done
--- src/run-XGBoost-libact.sh
+++ src/run-XGBoost-libact.sh
@@ -3,15 +3,15 @@ TASK="XGBoost";  # task-oriented model
 QUERY="XGBoost";  # query-oriented model
 SEED="0";  # starting seed
 N_EXP="110";  # number of experiments
-# For multi-processing
-N_JOBS="5";  # number of process, depending on your resources and algorithms
 END=$(($SEED + $N_EXP - 1));  # ending seed
+# For multi-processing
+N_JOBS="10";  # number of process, depending on your resources and algorithms
 
 TOOL="libact";
 
 echo "Small datasets!"
-for data in "appendicitis" "sonar" "parkinsons" "ex8b" "heart" "haberman" "ionosphere" "clean1" "breast" "wdbc" "australian" "diabetes" "mammographic" "ex8a" "tic" "german" "splice" "gcloudb" "gcloudub" "checkerboard"; do
-  for QS in "qbc-zhan" "dwus-zhan" "kcenter-zhan" "hintsvm-zhan" "albl-zhan" "quire-zhan" "vr-zhan"; do
+for QS in "qbc-zhan" "dwus-zhan" "kcenter-zhan" "hintsvm-zhan" "albl-zhan" "quire-zhan" "vr-zhan"; do
+  for data in "appendicitis" "sonar" "parkinsons" "ex8b" "heart" "haberman" "ionosphere" "clean1" "breast" "wdbc" "australian" "diabetes" "mammographic" "ex8a" "tic" "german" "splice" "gcloudb" "gcloudub" "checkerboard"; do
     echo "Start $QS with $QUERY on $data, repeated $N_EXP times with $N_JOBS process."
       for s in $(seq $SEED $N_JOBS $END); do #
         timeout 25920 python main.py --data_set $data --qs_name $QS --hs_name $QUERY --gs_name $TASK --seed $s --n_jobs $N_JOBS --n_trials $N_JOBS --tool $TOOL;
@@ -19,14 +19,15 @@ for data in "appendicitis" "sonar" "parkinsons" "ex8b" "heart" "haberman" "ionos
   done
 done
 
-N_EXP="10";  # number of experiments
-BUDGET="3000";
 echo "Large datasets!"
-for data in "spambase" "banana" "phoneme" "ringnorm" "twonorm" "phishing"; do
-  for QS in "qbc-zhan" "dwus-zhan" "kcenter-zhan" "hintsvm-zhan" "albl-zhan" "quire-zhan" "vr-zhan"; do
-    echo "Start $QS with $QUERY on $data, repeated $N_EXP times with $N_JOBS process."
+N_EXP="10";  # number of experiments
+END=$(($SEED + $N_EXP - 1));  # ending seed
+BUDGET="3000";
+for QS in "qbc-zhan" "dwus-zhan" "kcenter-zhan" "hintsvm-zhan" "albl-zhan" "quire-zhan" "vr-zhan"; do
+  for data in "spambase" "banana" "phoneme" "ringnorm" "twonorm" "phishing" "pol" "bioresponse" "covertype"; do
+    echo "Start $QS with $QUERY on $data limited budget $BUDGET, repeated $N_EXP times with $N_JOBS process."
       for s in $(seq $SEED $N_JOBS $END); do #
-        timeout 259200 python main.py --data_set $data --qs_name $QS --total_budget $BUDGET --hs_name $QUERY --gs_name $TASK --total_budget 3000 --seed $s --n_jobs $N_JOBS --n_trials $N_JOBS --tool $TOOL;
+        timeout 259200 python main.py --data_set $data --qs_name $QS --total_budget $BUDGET --hs_name $QUERY --gs_name $TASK --seed $s --n_jobs $N_JOBS --n_trials $N_JOBS --tool $TOOL;
       done
   done
 done
