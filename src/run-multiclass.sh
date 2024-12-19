2dc7c2588d402c335c14487ea62426568ecbbfba
ariapoy <ariapoy@gmail.com>
2024-12-19 21:12:25 +0800

    update exps for multi-class

--- src/run-multiclass.sh
+++ src/run-multiclass.sh
@@ -15,13 +15,13 @@ INIT_LBL_SIZES=(30 50 30 60 70 80 70 30 100 20)
 
 ## scikital
 TOOL="scikital";
-# for QS in "skal_uniform" "skal_us_margin" "skal_us_ent" "skal_us_lc" "skal_bald" "skal_coreset"; do
-for QS in "skal_uniform" "skal_us_margin" "skal_us_ent" "skal_us_lc" "skal_coreset"; do
-    for (( i=0; i<${#DATASETS[*]}; ++i)); do
-      echo "Exps for $QS on ${DATASETS[$i]} with $QUERY x $TASK";
-      for s in $(seq $SEED $N_JOBS $END); do #
-        timeout 86400 python main.py --data_set "${DATASETS[$i]}" --qs_name $QS --hs_name $QUERY --gs_name $TASK --seed $s --init_lbl_size "${INIT_LBL_SIZES[$i]}" --n_jobs $N_JOBS --n_trials $N_JOBS --tool $TOOL --scale --total_budget "3000";
-        # echo "python main.py --data_set "${DATASETS[$i]}" --qs_name $QS --hs_name $QUERY --gs_name $TASK --seed $s --init_lbl_size "${INIT_LBL_SIZES[$i]}" --n_jobs $N_JOBS --n_trials $N_JOBS --tool $TOOL --scale;"
-      done
+  # for QS in "skal_uniform" "skal_us_margin" "skal_bald" "skal_coreset" "skal_us_ent" "skal_us_lc"; do
+for (( i=0; i<${#DATASETS[*]}; ++i)); do
+  for QS in "skal_uniform" "skal_us_margin" "skal_coreset" "skal_us_ent" "skal_us_lc"; do
+    echo "Exps for $QS on ${DATASETS[$i]} with $QUERY x $TASK";
+    for s in $(seq $SEED $N_JOBS $END); do #
+      timeout 86400 python main.py --data_set "${DATASETS[$i]}" --qs_name $QS --hs_name $QUERY --gs_name $TASK --seed $s --init_lbl_size "${INIT_LBL_SIZES[$i]}" --n_jobs $N_JOBS --n_trials $N_JOBS --tool $TOOL --scale --total_budget "3000";
+      # echo "python main.py --data_set "${DATASETS[$i]}" --qs_name $QS --hs_name $QUERY --gs_name $TASK --seed $s --init_lbl_size "${INIT_LBL_SIZES[$i]}" --n_jobs $N_JOBS --n_trials $N_JOBS --tool $TOOL --scale;"
     done
+  done
 done
