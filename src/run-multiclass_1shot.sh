c483aaeb77f25924a2fad5d9df4764303ee5752d
d09944015 <d09944015@ws5.csie.ntu.edu.tw>
2024-12-21 12:12:35 +0800

    Add One-shot & Ten-Shot exps

---
+++ src/run-multiclass_1shot.sh
@@ -0,0 +1,27 @@
+# src/run-multiclass.sh
+
+TASK="LR" # "XGBoost";  # task-oriented model
+QUERY="LR" # "XGBoost";  # query-oriented model
+# exp settings
+SEED="0";
+N_EXP="110";
+END=$(($SEED + $N_EXP - 1));
+# For multi-processing
+N_JOBS="10";
+
+# list datasets and init_lbl_size as array
+DATASETS=("iris" "vehicle" "wine" "satellite" "winequality" "myocardial" "bean" "academic" "cifar10" "imdb")
+INIT_LBL_SIZES=(3 5 3 6 7 8 7 3 10 2)
+
+## scikital
+TOOL="scikital";
+  # for QS in "skal_uniform" "skal_us_margin" "skal_bald" "skal_coreset" "skal_us_ent" "skal_us_lc"; do
+for (( i=0; i<${#DATASETS[*]}; ++i)); do
+  for QS in "skal_uniform" "skal_us_margin" "skal_coreset" "skal_us_ent" "skal_us_lc"; do
+    echo "One-Shot Exps for $QS on ${DATASETS[$i]} with $QUERY x $TASK";
+    for s in $(seq $SEED $N_JOBS $END); do #
+      timeout 86400 python main.py --data_set "${DATASETS[$i]}" --qs_name $QS --hs_name $QUERY --gs_name $TASK --seed $s --init_lbl_size "${INIT_LBL_SIZES[$i]}" --n_jobs $N_JOBS --n_trials $N_JOBS --tool $TOOL --scale --nShot --total_budget "3000";
+      # echo "python main.py --data_set "${DATASETS[$i]}" --qs_name $QS --hs_name $QUERY --gs_name $TASK --seed $s --init_lbl_size "${INIT_LBL_SIZES[$i]}" --n_jobs $N_JOBS --n_trials $N_JOBS --tool $TOOL --scale;"
+    done
+  done
+done
