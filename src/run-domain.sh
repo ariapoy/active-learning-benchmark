1f68adc290eb843d8c1ceb3f40471d016dfdb1b1
ariapoy <ariapoy@gmail.com>
2025-02-08 07:41:48 +0800

    update exps

--- src/run-domain.sh
+++ src/run-domain.sh
@@ -4,21 +4,21 @@ TASK="XGBoost" # "XGBoost";  # task-oriented model
 QUERY="XGBoost" # "XGBoost";  # query-oriented model
 # exp settings
 SEED="0";
-N_EXP="110";
+N_EXP="10";
 END=$(($SEED + $N_EXP - 1));
 # For multi-processing
 N_JOBS="5";
 
 # list datasets and init_lbl_size as array
-DATASETS=("iris" "wine" "satellite" "bean" "academic")
-INIT_LBL_SIZES=(30 30 60 70 30)
+DATASETS=("cifar10" "imdb")
+INIT_LBL_SIZES=(100 20)
 
 # scikital
 TOOL="scikital";
-for QS in "skal_uniform" "skal_us_margin" "skal_bald" "skal_coreset" "skal_us_ent" "skal_us_lc"; do
+for QS in "skal_uniform" "skal_us_margin" "skal_bald" "skal_coreset"; do
   for (( i=0; i<${#DATASETS[*]}; ++i)); do
-    echo "Exps for $QS on ${DATASETS[$i]} with $QUERY x $TASK";
     for s in $(seq $SEED $N_JOBS $END); do #
+      echo "Exps $s for $QS on ${DATASETS[$i]} with $QUERY x $TASK";
       timeout 86400 python main.py --data_set "${DATASETS[$i]}" --qs_name $QS --hs_name $QUERY --gs_name $TASK --seed $s --init_lbl_size "${INIT_LBL_SIZES[$i]}" --n_jobs $N_JOBS --n_trials $N_JOBS --tool $TOOL --scale --total_budget "3000" --init_lbl_type "RS" --batch_size "1" --hyperparams_type "default";
     done
   done
@@ -26,10 +26,10 @@ done
 
 # google
 TOOL="google";
-QS="MCM";
+QS="mcm";
 for (( i=0; i<${#DATASETS[*]}; ++i)); do
-  echo "Exps for $QS on ${DATASETS[$i]} with $QUERY x $TASK";
   for s in $(seq $SEED $N_JOBS $END); do #
+    echo "Exps $s for $QS on ${DATASETS[$i]} with $QUERY x $TASK";
     timeout 86400 python main.py --data_set "${DATASETS[$i]}" --qs_name $QS --hs_name $QUERY --gs_name $TASK --seed $s --init_lbl_size "${INIT_LBL_SIZES[$i]}" --n_jobs $N_JOBS --n_trials $N_JOBS --tool $TOOL --scale --total_budget "3000" --init_lbl_type "RS" --batch_size "1" --hyperparams_type "default";
   done
 done
--- src/run-multiclass_1shot.sh
+++ src/run-multiclass_1shot.sh
@@ -7,21 +7,29 @@ SEED="0";
 N_EXP="110";
 END=$(($SEED + $N_EXP - 1));
 # For multi-processing
-N_JOBS="10";
+N_JOBS="5";
 
 # list datasets and init_lbl_size as array
-DATASETS=("iris" "vehicle" "wine" "satellite" "winequality" "myocardial" "bean" "academic" "cifar10" "imdb")
-INIT_LBL_SIZES=(3 5 3 6 7 8 7 3 10 2)
+DATASETS=("iris" "vehicle" "wine" "satellite" "winequality" "bean" "academic")
+INIT_LBL_SIZES=(3 5 3 6 7 7 3)
 
-## scikital
+# scikital
 TOOL="scikital";
-  # for QS in "skal_uniform" "skal_us_margin" "skal_bald" "skal_coreset" "skal_us_ent" "skal_us_lc"; do
-for (( i=0; i<${#DATASETS[*]}; ++i)); do
-  for QS in "skal_uniform" "skal_us_margin" "skal_coreset" "skal_us_ent" "skal_us_lc"; do
+for QS in "skal_uniform" "skal_us_margin" "skal_bald" "skal_coreset" "skal_us_ent" "skal_us_lc"; do
+  for (( i=0; i<${#DATASETS[*]}; ++i)); do
     echo "One-Shot Exps for $QS on ${DATASETS[$i]} with $QUERY x $TASK";
     for s in $(seq $SEED $N_JOBS $END); do #
       timeout 86400 python main.py --data_set "${DATASETS[$i]}" --qs_name $QS --hs_name $QUERY --gs_name $TASK --seed $s --init_lbl_size "${INIT_LBL_SIZES[$i]}" --n_jobs $N_JOBS --n_trials $N_JOBS --tool $TOOL --scale --total_budget "3000" --init_lbl_type "nShot" --batch_size "1" --hyperparams_type "default";
-      # echo "python main.py --data_set "${DATASETS[$i]}" --qs_name $QS --hs_name $QUERY --gs_name $TASK --seed $s --init_lbl_size "${INIT_LBL_SIZES[$i]}" --n_jobs $N_JOBS --n_trials $N_JOBS --tool $TOOL --scale;"
     done
+  done
+done
+
+# google
+TOOL="google";
+QS="mcm"
+for (( i=0; i<${#DATASETS[*]}; ++i)); do
+  echo "One-Shot Exps for $QS on ${DATASETS[$i]} with $QUERY x $TASK";
+  for s in $(seq $SEED $N_JOBS $END); do #
+    timeout 86400 python main.py --data_set "${DATASETS[$i]}" --qs_name $QS --hs_name $QUERY --gs_name $TASK --seed $s --init_lbl_size "${INIT_LBL_SIZES[$i]}" --n_jobs $N_JOBS --n_trials $N_JOBS --tool $TOOL --scale --total_budget "3000" --init_lbl_type "nShot" --batch_size "1" --hyperparams_type "default";
   done
 done
